7a1045aafcd6307145efd7b07c8d96e5c9b103ea
Adam Gunn <adamgunnadamgunn@gmail.com>
2021-11-11 18:17:04 -0500

    working on spotify poll dropdown

--- src/Navbar.js
+++ src/Navbar.js
@@ -25,8 +25,43 @@ function Navbar(props) {
             { className: "navbar_link" },
             React.createElement(
                 "a",
-                { href: "../new-poll" },
-                "New Poll"
+                { className: "create_poll_dropdown" },
+                "Create Poll"
+            ),
+            React.createElement(
+                "ul",
+                { className: "create_poll_dropdown_links" },
+                React.createElement(
+                    "li",
+                    { className: "navbar_link" },
+                    React.createElement(
+                        "a",
+                        null,
+                        "Regular Poll"
+                    )
+                ),
+                React.createElement(
+                    "li",
+                    { className: "navbar_link" },
+                    React.createElement(
+                        "a",
+                        { href: "../new-album-poll" },
+                        "Album Poll"
+                    ),
+                    React.createElement(
+                        "svg",
+                        {
+                            width: "16",
+                            height: "16",
+                            fill: "currentColor",
+                            className: "music_note",
+                            viewBox: "0 0 16 16"
+                        },
+                        React.createElement("path", { d: "M9 13c0 1.105-1.12 2-2.5 2S4 14.105 4 13s1.12-2 2.5-2 2.5.895 2.5 2z" }),
+                        React.createElement("path", { "fill-rule": "evenodd", d: "M9 3v10H8V3h1z" }),
+                        React.createElement("path", { d: "M8 2.82a1 1 0 0 1 .804-.98l3-.6A1 1 0 0 1 13 2.22V4L8 5V2.82z" })
+                    )
+                )
             )
         )
     );
--- src_dev/Navbar.js
+++ src_dev/Navbar.js
@@ -8,7 +8,26 @@ function Navbar(props) {
                 <a href="../about">About</a>
             </li>
             <li className="navbar_link">
-                <a href="../new-poll">New Poll</a>
+                <a className="create_poll_dropdown">Create Poll</a>
+                <ul className="create_poll_dropdown_links">
+                    <li className="navbar_link">
+                        <a>Regular Poll</a>
+                    </li>
+                    <li className="navbar_link">
+                        <a href="../new-album-poll">Album Poll</a>
+                        <svg
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="music_note"
+                            viewBox="0 0 16 16"
+                        >
+                            <path d="M9 13c0 1.105-1.12 2-2.5 2S4 14.105 4 13s1.12-2 2.5-2 2.5.895 2.5 2z" />
+                            <path fill-rule="evenodd" d="M9 3v10H8V3h1z" />
+                            <path d="M8 2.82a1 1 0 0 1 .804-.98l3-.6A1 1 0 0 1 13 2.22V4L8 5V2.82z" />
+                        </svg>
+                    </li>
+                </ul>
             </li>
         </ul>
     );
